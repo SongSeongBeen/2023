415710a8751ee34904858462dc0af8162f991efb
송성빈 <aktm1004@naver.com>
2021-11-16 15:35:36 +0900

    콘솔입력

--- src/com/javaex/ex02/Ex02.java
+++ src/com/javaex/ex02/Ex02.java
@@ -3,19 +3,16 @@ package com.javaex.ex02;
 public class Ex02 {
 	
 	public static void main(String[] args) {
-		
-		 String name ="손인혜";
-	      
-	      System.out.println("안녕\"하\"세요");
-	      
-	      System.out.println("안녕하세요");
-	      System.out.println("안녕\t세요");
-	      System.out.println("안녕\n세요");
-	      System.out.println("안녕하세요");
-	      
-	      System.out.println("제이름은 "+name+" 입니다");
 
-		
+		String name = "송성빈";
+
+		System.out.println("안녕\"하\"세요");
+		System.out.println("안녕하세요");
+		System.out.println("안녕\t세요");
+		System.out.println("안녕\n세요");
+		System.out.println("안녕하세요");
+
+		System.out.println("제이름은 " + name + " 입니다");
 	}
 
 }
