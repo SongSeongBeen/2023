d11d84d840b43093a470735e2379e35dfe4b5146
송성빈 <aktm1004@naver.com>
2021-11-16 14:14:42 +0900

    과제

--- src/com/javaex/ex03/Ex07.java
+++ src/com/javaex/ex03/Ex07.java
@@ -10,17 +10,29 @@ public class Ex07 {
 		System.out.print("점수를 입력하세요: ");
 		
 		int a = sc.nextInt();
+//		boolean b = (90>a && a >= 80);
+//		boolean c = (80>a && a >= 70);
+//		boolean d = (70>a && a >= 60);
 
 		if      (a >= 90) {
 			System.out.print("A등급");
 		} 
-		else if (a >= 80) {
+		else if (a >= 80){
+			  //(b)
+			  //(a >= 80)
+			  //(90>a && a >= 80)
 			System.out.print("B등급");
 		} 
 		else if (a >= 70) {
+			  //(c)
+		      //(a >= 70)
+			  //(80>a && a >= 70)
 			System.out.print("C등급");
 		} 
 		else if (a >= 60) {
+			  //(d)
+		      //(a >= 60)
+			  //(70>a && a >= 60)
 			System.out.print("D등급");
 		} 
 		else {
---
+++ src/com/javaex/ex03/Ex08.java
@@ -0,0 +1,31 @@
+package com.javaex.ex03;
+
+import java.util.Scanner;
+
+public class Ex08 {
+	
+	public static void main(String[]args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요");
+		System.out.print("숫자: ");
+		int a = sc.nextInt();
+		
+		if(a>0) {
+			if(a%2==0) {
+			System.out.print("짝수");
+			}
+			else {
+			System.out.print("홀수");
+			}
+		}
+		else if(a<0) {
+			System.out.print("음수");
+		}
+		else {
+			System.out.print("0");
+		}
+		sc.close();
+	}
+}
