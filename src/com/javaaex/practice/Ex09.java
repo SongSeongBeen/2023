a4c7b0549abc897f4dc5fdd138cc672af499264c
송성빈 <aktm1004@naver.com>
2021-11-18 22:30:06 +0900

    과제3

--- src/com/javaaex/practice/Ex09.java
+++ src/com/javaaex/practice/Ex09.java
@@ -4,15 +4,14 @@ public class Ex09 {
 	
 	public static void main(String[]args) {
 		
-		int num = 1;
-		
-		for(int y=0; y<10; y++) { //세로
+		for(int y=1; y<=10; y++) { //세로
 			
-			for(int x=1; x<=10; x++) { // 가로
-				y=y+1;
-				System.out.print(y+"\t");
+			for(int x=0; x<10; x++) { // 가로
+				
+				System.out.print(y+x+"\t");
 			}
-			System.out.print("");
+			
+			System.out.println("");
 		}
 	}
 
