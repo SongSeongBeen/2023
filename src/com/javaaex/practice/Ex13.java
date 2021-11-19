39092a8f75524db9f86c824477ebc10fca361b74
송성빈 <aktm1004@naver.com>
2021-11-20 02:29:59 +0900

    과제3

---
+++ src/com/javaaex/practice/Ex13.java
@@ -0,0 +1,23 @@
+package com.javaaex.practice;
+
+import java.util.Scanner;
+
+public class Ex13 {
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요: ");
+		int num = sc.nextInt();
+		
+		int sum = 0;
+		
+		for (int i = 1; i <= num; i++) {// i는 입력한 수 까지만
+			sum+=i;//sum=sum+i   //0=0+1
+		}                        //1=1+2 ... 3=3+3 
+		System.out.println("합계:" + sum);
+
+		sc.close();
+	}
+}
