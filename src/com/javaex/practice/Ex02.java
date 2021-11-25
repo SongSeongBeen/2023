e12345ed0a391716520f1c47bd9299af0ed9ddb5
송성빈 <aktm1004@naver.com>
2021-11-25 18:34:56 +0900

    과제4

--- src/com/javaex/practice/Ex02.java
+++ src/com/javaex/practice/Ex02.java
@@ -1,5 +1,18 @@
 package com.javaex.practice;
 
 public class Ex02 {
+	
+	public static void main(String[] args) {
+	
+		double[] arrA  = new double[] {1.2, 3.3, 6.7};
+	//	double[] reversearrA = new double[3]; //거꾸로된 배열값
+		
+		for(int i=2; i>-1; i--) {       // 방 2 부터 0까지 역순
+			System.out.println(arrA[i]);
+		}
+	
+	}
 
-}
+   }
+
+
