702ac64500c204c931734c1cf9ed2b4f3797642b
송성빈 <aktm1004@naver.com>
2021-11-30 18:17:25 +0900

    과제6

--- src/com/javaex/ex04/UserApp.java
+++ src/com/javaex/ex04/UserApp.java
@@ -3,8 +3,25 @@ package com.javaex.ex04;
 public class UserApp {
 
 	public static void main(String[] args) {
-		
 
+		Customer[] cArray = new Customer[3];
+
+		Customer c01 = new Customer("jws", "1234", "정우성", 1000);
+
+		Customer c02 = new Customer("yjs", "2345", "이효리", 2000);
+
+		Customer c03 = new Customer("master", "m7788", "운영자", 5000000);
+
+		cArray[0] = c01;
+		cArray[1] = c02;
+		cArray[2] = c03;
+
+		for (int i = 0; i < cArray.length; i++) {
+			cArray[i].showInfo();
+		}
+
+		Employee y01 = new Employee(5000000);
+		y01.showInfo();
 	}
 
 }
--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -2,16 +2,31 @@ package com.javaex.ex07;
 
 public class SmartPhone extends MusicPhone {
     
-    public void execute(String str){
-        
-        //코드작성
-        
-    }
- 
-    //메소드작성
-    
-    //메소드작성
-    
-    
-    
+	public void execute(String str) {
+
+		if ("앱".equals(str)) {
+			app();
+		} else // if("음악".equals(str)){
+				// playMusic();
+			super.execute(str);
+	}
+
+	// 메소드작성
+	private void app() {
+		System.out.println("앱실행");
+	}
+
+	// 메소드작성
+	protected void playMusic() {
+		System.out.println("다운로드해서 음악재생");
+	}
+
 }
+ 
+    
+    
+    
+    
+    
+    
+
