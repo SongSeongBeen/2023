17fbf6c235d36c83b01485882ccf8dd638b088c3
송성빈 <aktm1004@naver.com>
2021-12-01 17:33:30 +0900

    과제5

--- src/com/javaex/ex02/Goods.java
+++ src/com/javaex/ex02/Goods.java
@@ -1,6 +1,39 @@
 package com.javaex.ex02;
 
 public class Goods {
+	
+	//필드
+	private String name;
+	private int price;
+	//생성자
+	
+	public Goods() { //쓰지않으면 없어진다 
+		
+	}
+	public Goods(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	//메소드 g/s	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getPrice() {
+		return price;
+	}
+	
+	//일반 메소드
+	public void showInfo() {
+		System.out.println("상품명: " + name + ", 가격: " + price);
+	}
 
 	
 	
--- src/com/javaex/ex03/Song.java
+++ src/com/javaex/ex03/Song.java
@@ -1,8 +1,87 @@
 package com.javaex.ex03;
 
 public class Song {
+	//필드
+	private String title;
+	private String artist;
+	private String album;
+	private String composer;
+	private String year;
+	private int track;
 	
+	//생성자
+	public Song() { //디폴트 생성자
+		System.out.println("Song(파라미터0개)");
+	}
 	
+	public Song(String title, String artist, String album,  String composer, String year) {
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+		this.composer = composer;
+		this.year = year;
+		System.out.println("Song(파라미터5개)");
+	}
+	
+	public Song(String title, String artist, String album,  String composer, String year, int track) {
+		this(title, artist, album, composer, year);
+		this.track = track;
+		System.out.println("Song(파라미터6개)");
+	}
+	//메소드g/s
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getTitle() {
+		return title;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setComposer(String composer) {
+		this.composer = composer;
+	}
+	public String getComposer() {
+		return composer;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getYear() {
+		return year;
+	}
+
+	public void setTrack(int track) {
+		this.track = track;
+	}
+	public int getTrack() {
+		return track;
+	}
+	//일반메소드
+
+//	@Override
+//	public String toString() {
+//		return "Song [title=" + title + ", artist=" + artist + ", album=" + album + ", composer=" + composer + ", year="
+//				+ year + ", track=" + track + "]";
+//	}
+//	
+//	public void showInfo() {
+//		System.out.println(title + artist + album + composer + year + track);
+//	}
 	
 }
 
