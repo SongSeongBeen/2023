0846706f6ad296f3bcfeabf9621c1773e8e83a62
송성빈 <aktm1004@naver.com>
2021-12-01 22:51:03 +0900

    과제7

--- src/com/javaex/ex01/Shape.java
+++ src/com/javaex/ex01/Shape.java
@@ -1,6 +1,6 @@
 package com.javaex.ex01;
 
-public abstract class Shape {
+public abstract class Shape { //코드만입력
 	
 	protected String fillColor;
 	
--- src/com/javaex/ex01/ShapeApp.java
+++ src/com/javaex/ex01/ShapeApp.java
@@ -4,15 +4,25 @@ public class ShapeApp {
 
 	public static void main(String[] args) {
 
-		Shape s = new Shape("빨강");
+		Shape[] sArray = new Shape[3];
+		
+		//Shape s = new Shape("빨강"); // abstractd class
 		Circle c1 = new Circle("녹색", 10);
-		Ractangle r1 = new Ractangle("빨강", 4, 4);
-		Shape sr1 = new Ractangle("빨강", 4, 4);
+		Rectangle r1 = new Rectangle("빨강", 4, 4);
+		Rectangle sr1 = new Rectangle("빨강", 12, 10);
+			
+		sArray[0] = c1;
+		sArray[1] = r1;
+		sArray[2] = sr1;
+		
+		for(int i=2; i<sArray.length; i++) {
+			sArray[i].draw();
+		}
 		
 		//sr1을 그리는 메소드 호출
-		
+		//((Ractangle)sr1).draw();
 		//sr1의 가로크기 출력
-		
+		//System.out.println("sr1의 가로는 "+((Rectangle)sr1.setWidth()+" 입니다.");
 	}
 }
 
