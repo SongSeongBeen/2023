91129966bf41405f69d3cc86144475c36497c190
송성빈 <aktm1004@naver.com>
2021-12-08 17:03:52 +0900

    I/O 스트림

---
+++ src/com/javaex/ex01/Ex01.java
@@ -0,0 +1,44 @@
+package com.javaex.ex01;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Ex01 {
+
+	public static void main(String[] args) throws Exception {
+		
+		InputStream in = new FileInputStream("C:\\JavaStudy\\file\\img.jpg"); //생성자에 파일위치를 써야한다.
+		OutputStream out = new FileOutputStream("C:\\JavaStudy\\file\\byteimg.jpg");
+			 
+		byte[] buff = new byte[1234]; //byte로 정해줌
+		while(true) {
+			
+			int data = in.read(buff);
+			
+			if(data == -1) {
+				System.out.println("복사가 완료되었습니다." + data);
+				
+				
+				break;
+			}
+			out.write(buff);
+		}
+		
+		
+		
+//		System.out.println("복사시작");
+//		while(true) {
+//			int data = in.read();
+//			
+//			if(data == -1) {
+//				System.out.println("복사가 완료 되었습니다." + data);
+//				break;
+//			}
+//			out.write(data);
+//		}
+		in.close();
+		out.close();
+	}
+}
