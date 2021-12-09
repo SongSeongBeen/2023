3d75a31751f234eb0cb25756a0a1e56cd8434969
송성빈 <aktm1004@naver.com>
2021-12-09 21:47:16 +0900

    I/O 스트림 예제 수정

--- src/com/javaex/ex06/PhoneApp.java
+++ src/com/javaex/ex06/PhoneApp.java
@@ -1,9 +1,12 @@
 package com.javaex.ex06;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -20,31 +23,39 @@ public class PhoneApp {
 			//파일에 접속
 			Reader fr = new FileReader("F:\\javaStudy\\file\\PhoneDB.txt");
 			BufferedReader br = new BufferedReader(fr);
-			
+			//저장 경로
+			Writer fw = new FileWriter("F:\\javaStudy\\file\\PhoneDB.txt");
+			BufferedWriter bw = new BufferedWriter(fw);
 			
 			for(int i=0; i<3; i++) {
 				String put = sc.nextLine();
 				String[] sArray = put.split(" ");
 
 				Person f = new Person(sArray[0], sArray[1], sArray[2]);
+				
+				pList.add(f);
+			}
+			System.out.println("데이터를 입력하세요");
+			
+			for(int i=0; i<1; i++) {
+				String put = sc.nextLine();
+				String[] sArray = put.split(" ");
+
 				String name = sArray[0];
 				String  hp = sArray[1];
 				String  company = sArray[2];
+				
 				Person p01 = new Person(name, hp, company);
 				pList.add(p01);
 			}
 			for(Person f:pList) {
 	    		f.showInfo();
 			}
-			
-			
-			
-			
-			
-			
 			sc.close();
-			
 			br.close();
-		
+			bw.close();
 		}
+		
 }
+
+
