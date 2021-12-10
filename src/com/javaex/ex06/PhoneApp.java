c6edc397225ddbf25a4e7faa0a30a44f3b260cc8
송성빈 <aktm1004@naver.com>
2021-12-10 11:14:44 +0900

    I/O 스트림 예제 수정

--- src/com/javaex/ex06/PhoneApp.java
+++ src/com/javaex/ex06/PhoneApp.java
@@ -16,7 +16,6 @@ public class PhoneApp {
 		public static void main(String[] args) throws IOException {
 			
 			Scanner sc = new Scanner(System.in);
-			System.out.println("데이터를 입력하세요");
 			
 			//메모리 올리기
 			List<Person> pList = new ArrayList<Person>();
@@ -24,34 +23,77 @@ public class PhoneApp {
 			Reader fr = new FileReader("F:\\javaStudy\\file\\PhoneDB.txt");
 			BufferedReader br = new BufferedReader(fr);
 			//저장 경로
+
+
+			while (true) { //한줄씩 읽어
+				String line = br.readLine();
+				if(line ==null) {
+					break;
+				}
+				String[] sArray = line.split(",");  //마지막이 아니면,로
+				String name = sArray[0];
+				String  hp = sArray[1];
+				String  company = sArray[2];
+				//생성 값 넣기
+				Person p01 = new Person(name, hp, company);
+				//리스트에 추가
+				pList.add(p01);
+				
+			}
+		
+//			for(int i=0; i<3; i++) {
+//				String put = sc.nextLine();
+//				String[] sArray = put.split(",");
+//
+//				Person f = new Person(sArray[0], sArray[1], sArray[2]);
+//				
+//				pList.add(f);
+//			}
+		
+			//화면출력
+			for(int i=0; i<pList.size(); i++) {
+				System.out.println("이름:" + pList.get(i).getName());
+				System.out.println("핸드폰 :" + pList.get(i).getHp());
+				System.out.println("회사:" + pList.get(i).getCompany());
+				System.out.println(" ");
+				
+			}
+			//기보드값 입력
+			System.out.println("데이터를 입력하세요");
+			String put = sc.nextLine();
+			String[] sArray = put.split(","); //마지막 ,으로
+
+			String name = sArray[0];
+			String  hp = sArray[1];
+			String  company = sArray[2];
+			Person p01 = new Person(name, hp, company);
+			//리스트에 추가
+			pList.add(p01);
+			
+			//파일저장
 			Writer fw = new FileWriter("F:\\javaStudy\\file\\PhoneDB.txt");
 			BufferedWriter bw = new BufferedWriter(fw);
 			
-			for(int i=0; i<3; i++) {
-				String put = sc.nextLine();
-				String[] sArray = put.split(",");
-
-				Person f = new Person(sArray[0], sArray[1], sArray[2]);
-				
-				pList.add(f);
+			for(int i=0; i<pList.size(); i++) {
+				String str = pList.get(i).getName() + "," + pList.get(i).getHp() + "," +  pList.get(i).getCompany();
+				bw.write(str);
+				bw.newLine();
 			}
-			System.out.println("데이터를 입력하세요");
-			
-			for(int i=0; i<1; i++) {
-				String put = sc.nextLine();
-				String[] sArray = put.split(",");
-
+				
+//				String put = sc.nextLine();
+//				String[] sArray = put.split(",");
+//
 //				String name = sArray[0];
 //				String  hp = sArray[1];
 //				String  company = sArray[2];
 //				Person p01 = new Person(name, hp, company);
-				
-				Person p01 = new Person(sArray[0], sArray[1], sArray[2]);
-				pList.add(p01);
-			}
-			for(Person f:pList) {
-	    		f.showInfo();
-			}
+//				
+//				Person p01 = new Person(sArray[0], sArray[1], sArray[2]);
+//				pList.add(p01);
+//			}
+//			for(Person f:pList) {
+//	    		f.showInfo();
+//			}
 			sc.close();
 			br.close();
 			bw.close();
