e11069feffe6b64c8d9e1f10d77db174bbc1d5aa
송성빈 <aktm1004@naver.com>
2021-12-10 14:34:51 +0900

    네트워크 스트림

--- src/echo/ex01/Client.java
+++ src/echo/ex01/Client.java
@@ -1,13 +1,21 @@
 package echo.ex01;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class Client {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		Socket socket = new Socket();
-		socket.connect(new IneSocketAddress); 
+		System.out.println("<클라이언트 시작>");           
+		//3. 포트에 연결									 //포트값 자기가 설정
+		socket.connect(new InetSocketAddress("192.168.0.225", 10001)); //ipv4 주소 Server 동일주소
+		System.out.println("[연결을 기다리고 있습니다.]");
+		
+		System.out.println("[서버에 연결 되었습니다.]");
+		socket.close();
 	}
 	
 	
--- src/echo/ex01/Server.java
+++ src/echo/ex01/Server.java
@@ -8,12 +8,22 @@ import java.net.Socket;
 public class Server {
 
 	public static void main(String[] args) throws IOException { //예외처리 인풋아웃풋
-		
+		//1.메모리 올려라
 		ServerSocket serverSocket = new ServerSocket();
-		serverSocket.bind(new InetSocketAddress("192.168.0.1", 10001));
-		
+		//2.포트생성
+		serverSocket.bind(new InetSocketAddress("192.168.0.225", 10001)); //ipv4 주소
+		System.out.println("<서버시작>");
+		System.out.println("========================");
+		System.out.println("[연결을 기다리고 있습니다.]");
+		//4. 연결오면 동의하고 다음으로 넘겨줘라
 		Socket socket = serverSocket.accept();
 		
-	
+		System.out.println("[클라이언트가 연결되었습니다.]");
+		socket.close();
+		serverSocket.close();
+		
+		//cd c:/ -> cd javaStudy -> cd workspace -> cd chapter06 -> cd bin -> java echo.ex01.Client
+		//파일들어가서 바로 cmd치면 바로 생성된다.
 	}
+	
 }
