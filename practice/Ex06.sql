f7f1e60c49ab132cfff9589850390d2b18d62e5d
송성빈 <aktm1004@naver.com>
2021-12-26 17:35:51 +0900

    연습문제

--- practice/Ex06.sql
+++ practice/Ex06.sql
@@ -41,7 +41,7 @@ CREATE TABLE    book(
     title       varchar2(100),
     pubs        varchar2(100),
     pub_date    date,
-    author_id   number(20),
+    author_id   number(20) not null,
     PRIMARY KEY (book_id),
     CONSTRAINT  book_fk FOREIGN KEY (author_id)
     REFERENCES  author(author_id)
