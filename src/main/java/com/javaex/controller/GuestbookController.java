a802961ca3d8095d34f58a7a919644e6ee8a5666
송성빈 <aktm1004@naver.com>
2022-01-22 23:54:12 +0900

    mysite4

--- src/main/java/com/javaex/controller/GuestbookController.java
+++ src/main/java/com/javaex/controller/GuestbookController.java
@@ -1,28 +1,71 @@
 package com.javaex.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.javaex.dao.UserDao;
-import com.javaex.vo.UserVo;
+import com.javaex.dao.GuestbookDao;
+import com.javaex.vo.GuestbookVo;
 
 
 @Controller
 public class GuestbookController {
-
-	@Autowired
-	private UserDao userDao;
+		//필드
+		@Autowired
+		private GuestbookDao guestbookDao;
+		private GuestbookVo guestbookVo;
+		//생성자
 		
-//리스트입력-확인		
-		@RequestMapping(value="/guestList", method= {RequestMethod.GET, RequestMethod.POST})
-		public String loginForm(@ModelAttribute UserVo userVo) {
-			System.out.println("guestList");
-			userDao.getUser(userVo);
-			return "/guestbook/addList";
+		//메소드gs
+		
+		//메소드 일반
+		
+//리스트	
+		@RequestMapping(value = "/gList", method= {RequestMethod.GET, RequestMethod.POST})
+		public String gList(Model model) {
+				System.out.println("GuestbookController=addList");
+			
+			List<GuestbookVo> gList = guestbookDao.getList();
+			System.out.println(gList.toString());
+			
+			model.addAttribute("gList", gList);
+				
+			return "addList";
 		}
+	
+//리스트입력-확인
+		@RequestMapping(value = "/add", method= {RequestMethod.GET, RequestMethod.POST})
+		public String add(@ModelAttribute GuestbookVo guestboookVo) {
+			System.out.println("GuestbookController=add");
+			
+			guestbookDao.guestInsert(guestbookVo);
+		
+		return "redirect:./gList";
+		}
+		
+//삭제-폼
+		@RequestMapping(value = "/deleteForm", method= {RequestMethod.GET, RequestMethod.POST})
+		public String deleteForm(@ModelAttribute GuestbookVo guestboookVo) {
+			System.out.println("GuestbookController=deleteForm");
+			
+		return "deleteForm";
+		}
+//삭제
+		@RequestMapping(value = "/delete", method= {RequestMethod.GET, RequestMethod.POST})
+		public String delete(@ModelAttribute GuestbookVo guestboookVo) {
+			System.out.println("GuestbookController=delete");
+			
+			guestbookDao.guestbookDelete(guestboookVo);
+		
+			return "redirect:./gList";
+		}
+	
+
 }
 		
 
--- src/main/java/com/javaex/dao/GuestbookDao.java
+++ src/main/java/com/javaex/dao/GuestbookDao.java
@@ -1,9 +1,12 @@
 package com.javaex.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.javaex.vo.GuestbookVo;
 import com.javaex.vo.UserVo;
 
 @Repository
@@ -11,6 +14,8 @@ public class GuestbookDao {
 //공통영역	
 		@Autowired
 		private SqlSession sqlSession;
+		private int connt = 0;
+		
 //공통영역	
 		
 		public UserVo getUser(UserVo userVo) {
@@ -20,7 +25,31 @@ public class GuestbookDao {
 			UserVo authUser = sqlSession.selectOne("UserVo", userVo);
 			
 			return authUser;
+		}
+		
+//리스트 영역	
+		public List<GuestbookVo> getList(){
+			System.out.println("GuestbookDao.getList");
+			List<GuestbookVo> guestbookList = sqlSession.selectList("guestbook.selectList");
 			
+			return guestbookList;
+		
+		}
+		
+//등록		
+		public int guestInsert(GuestbookVo guestbookVo){
+			System.out.println("GuestbookDao.guestInser");
+			sqlSession.insert("guestbook.insert", guestbookVo);
+			System.out.println(connt +"건");
+			return connt;
+		
+		}
+		
+//삭제 영역
+		public int guestbookDelete(GuestbookVo guestBookVo) {
+			System.out.println("PhoneDao.personDelete");
+			sqlSession.delete("guestbook.delete", guestBookVo);
+			return connt;
 		}
 
 }
